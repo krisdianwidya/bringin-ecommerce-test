44ee8f5c58470f41c809cb4825636774d6ea1084
krisdianwidya <krisdianwidya@gmail.com>
2024-02-29 08:46:23 +0700

    init routing for pages - create Home, Cart, NotFound pages

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { PrimeReactProvider } from "primereact/api";
+import { BrowserRouter } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const value = {
+  appendTo: "self",
+};
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <PrimeReactProvider value={value}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </PrimeReactProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,5 @@
+const NotFound = () => {
+  return <h1>Not Founda</h1>;
+};
+
+export default NotFound;
